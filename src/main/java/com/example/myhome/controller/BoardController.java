20789013e1454eb7c6d0e65e2e3be33d9eb18325
seojonghon <76559811+seojonghon@users.noreply.github.com>
2023-04-12 20:49:48 +0900

    init

--- src/main/java/com/example/myhome/controller/BoardController.java
+++ src/main/java/com/example/myhome/controller/BoardController.java
@@ -1,15 +1,47 @@
 package com.example.myhome.controller;
-
+import org.springframework.boot.Banner;
+import org.springframework.ui.Model;
+import com.example.myhome.model.Board;
+import com.example.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping
+@RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
     @GetMapping("/list")
-public String list(){
+public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
         return "board/list";
     }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+            return "board/form";
+        }
+        return "board/form";
+    }
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board){
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
+
 }
+
+
+
