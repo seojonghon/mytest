dd17e0809e4fe93a834f201c759fc26b4bfa8d92
seojonghon <76559811+seojonghon@users.noreply.github.com>
2023-05-01 01:19:00 +0900

    init

--- src/main/java/com/example/myhome/controller/BoardController.java
+++ src/main/java/com/example/myhome/controller/BoardController.java
@@ -1,10 +1,17 @@
 package com.example.myhome.controller;
+import jakarta.validation.Valid;
+import com.example.myhome.validator.BoardValidator;
 import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.ui.Model;
 import com.example.myhome.model.Board;
 import com.example.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +25,15 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
     @GetMapping("/list")
-public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
-        model.addAttribute("boards",boards);
+    public String list(Model model, @PageableDefault(size = 5) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 
@@ -36,7 +49,11 @@ public String list(Model model){
         return "board/form";
     }
     @PostMapping("/form")
-    public String greetingSubmit(@ModelAttribute Board board){
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){
+        //boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()){
+            return "board/form";
+        }
         boardRepository.save(board);
         return "redirect:/board/list";
     }
--- src/main/java/com/example/myhome/model/Board.java
+++ src/main/java/com/example/myhome/model/Board.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Data
@@ -12,6 +14,8 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30)
     private String title;
     private String content;
 }
